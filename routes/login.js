52ae286227d651444cf91342abebe368663bb84c
msalosarito <msalosarito>
2023-05-23 13:48:31 +0800

    fifth

--- routes/login.js
+++ routes/login.js
@@ -5,36 +5,7 @@ const { PrismaClient } = require("@prisma/client");
 const crypto = require('crypto');
 const prisma = new PrismaClient();
 
-
-// Initialize the session middleware
-router.use(session({
-  secret: 'your-secret-key',
-  resave: false,
-  saveUninitialized: true
-}));
-
-
-/* GET home page. */
-router.get('/login', async function(req, res, next) {
-  // If user is already logged in, redirect to appropriate page
-  if (req.session.user) {
-    switch (req.session.user.usertype) {
-      case "Admin":
-        res.redirect("/admin");
-        break;
-      case "Manager":
-        res.redirect("/manager");
-        break;
-      case "User":
-        res.redirect("/users");
-        break;
-      default:
-        res.status(400).send("Invalid userType");
-    }
-    return;
-  }
-
-  // Otherwise, render the login page
+router.get('/login', function(req, res, next) {
   res.render('login', { title: 'Login' });
 });
 
@@ -56,8 +27,8 @@ router.post('/login', async function(req, res, next) {
 
     if (user.password === encryptedPassword) {
 
-      // Store the user object in the session
-      req.session.user = user;
+      // Store the user ID in the session
+      req.session.userId = user.id;
 
       // Redirect user based on their userType
       switch (user.usertype) {
@@ -80,18 +51,6 @@ router.post('/login', async function(req, res, next) {
     console.error(error);
     res.status(500).send("Something went wrong");
   }
-});
-
-
-/* GET logout page. */
-router.get('/logout', function(req, res, next) {
-  req.session.destroy(err => {
-    if (err) {
-      console.error(err)
-    } else {
-      res.redirect('/login')
-    }
-  })
 });
 
 module.exports = router;
